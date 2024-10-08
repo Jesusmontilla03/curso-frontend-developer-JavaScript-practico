a1a46e7c3278180965a852b3adac84febff8e0ce
jesusmontilla03 <jsdmontilla@gmail.com>
2024-10-07 23:37:51 -0400

    Solucion a funcion de añadir o remover la clase active a los selectores

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ const menuMobile = document.querySelector('.mobile-menu');
 classActiveToggle(btnEmail, desktopMenu);
 classActiveToggle(btnMenuMobile, menuMobile);
 
-selectorList = [desktopMenu, menuMobile];
+let selectorList = [desktopMenu, menuMobile];
 
 function classActiveToggle (btn, selectorElement) {
     btn.addEventListener('click', () => {
+
+        let selectorFilter = selectorList.indexOf(selectorElement);
+        selectorList.splice(selectorFilter, 1);
+        // Se filtra el selector usado en la funcion para no ser afectado por el forEach.
 
         selectorList.forEach( selector => {
             selector.classList.remove('active')
         });
 
         selectorElement.classList.toggle('active');
+        selectorList.push(selectorElement);
+        // Se agrega nuevamente el selector filtrado para ser usado correctamente luego.
     })
 }
